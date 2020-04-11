80b153fee446c5a2458a33d43078c37420966313
reebdeeb <rebekatwalker@gmail.com>
2020-04-10 17:29:42 -0700

    Organized code by giving update_bullets its own function

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,12 +27,7 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
-			self.bullets.update()
-
-			#Get rid of bullets that have disappeared
-			for bullet in self.bullets.copy():
-				if bullet.rect.bottom <=0:
-					self.bullets.remove(bullet)
+			self._update_bullets()
 			self._update_screen()
 
 	def _check_events(self):
@@ -70,6 +65,15 @@ class AlienInvasion:
 		if len(self.bullets) < self.settings.bullets_allowed:
 			new_bullet = Bullet(self)
 			self.bullets.add(new_bullet)
+
+	def _update_bullets(self):
+		"""Update position of bullets and get rid of old bullets"""
+		self.bullets.update()
+
+		#Get rid of bullets that have disappeared
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <=0:
+				self.bullets.remove(bullet)
 
 	def _update_screen(self):
 
