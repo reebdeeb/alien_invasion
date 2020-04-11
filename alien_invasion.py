99c2d47fde8234dffac4a4fa470ba8e289595925
reebdeeb <rebekatwalker@gmail.com>
2020-04-10 17:23:42 -0700

    Deleted old bullets and limited the number of bullets on screen to 3

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,11 @@ class AlienInvasion:
 			self._check_events()
 			self.ship.update()
 			self.bullets.update()
+
+			#Get rid of bullets that have disappeared
+			for bullet in self.bullets.copy():
+				if bullet.rect.bottom <=0:
+					self.bullets.remove(bullet)
 			self._update_screen()
 
 	def _check_events(self):
@@ -62,8 +67,9 @@ class AlienInvasion:
 
 	def _fire_bullet(self):
 		"""Create a new bullet and add it to the bullets group"""
-		new_bullet = Bullet(self)
-		self.bullets.add(new_bullet)
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
 
 	def _update_screen(self):
 
