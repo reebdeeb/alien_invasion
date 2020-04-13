c6cbb706897664d1f6b15e95400364d4cede7b38
reebdeeb <rebekatwalker@gmail.com>
2020-04-13 10:31:10 -0700

    Created alien fleet class and placement on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien 
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behavior."""
@@ -21,6 +22,9 @@ class AlienInvasion:
 
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
+		self.aliens = pygame.sprite.Group()
+
+		self._create_fleet()
 
 	def run_game(self):
 		"""Start the main loop for the game."""
@@ -75,13 +79,30 @@ class AlienInvasion:
 			if bullet.rect.bottom <=0:
 				self.bullets.remove(bullet)
 
+	def _create_fleet(self):
+		"""Create the fleet of aliens"""
+		#Make an alien and find the number of aliens in a row
+		alien = Alien(self)
+		alien_width = alien.rect.width
+		available_space_x = self.settings.screen_width - (2 * alien_width)
+		number_aliens_x = available_space_x // (2 * alien_width)
+
+		#Create the first row of aliens
+		for alien_number in range(number_aliens_x):
+			#Create and alien and place it in a row
+			alien = Alien(self)
+			alien.x = alien_width + 2 * alien_width * alien_number
+			alien.rect.x = alien.x
+			self.aliens.add(alien)
+
 	def _update_screen(self):
 
 		#Redraw screen during each pass through the loop
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()	
 		for bullet in self.bullets.sprites():
-			bullet.draw_bullet()	
+			bullet.draw_bullet()
+		self.aliens.draw(self.screen)	
 
 		#Make the most recently drawn screen visible
 		pygame.display.flip()	
--- bullet.py
+++ bullet.py
@@ -11,7 +11,7 @@ class Bullet(Sprite):
 		self.settings = ai_game.settings
 		self.color = self.settings.bullet_color
 
-		#Create a bullet rect a (0, 0) and then set correct position
+		#Create a bullet rect at (0, 0) and then set correct position
 		self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
 			self.settings.bullet_height)
 		self.rect.midtop = ai_game.ship.rect.midtop
